648c7d7f2536b20345a25a41c23c58134d4c4165
antoniovizarraga <antonio.vizarraga@iesnervion.es>
2025-05-12 01:39:04 +0200

    ASP creado

---
+++ InvernaderoASP/Controllers/InvernaderosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvernaderoASP.Controllers
+{
+    public class InvernaderosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: InvernaderosController
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(IFormCollection collection)
+        {
+            //TODO: Hacer que la vista retorne un NotFound si no existe el invernadero con la fecha seleccionada. O hacerlo por un ViewBag.
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
